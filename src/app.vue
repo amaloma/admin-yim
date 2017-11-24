f9cf8d70b3c008b9d40ce07bc66f5fa570477b80
Air <air-lidon@hotmail.com>
2017-11-24 11:49:05 +0700

    navbar icon

--- src/app.vue
+++ src/app.vue
@@ -60,12 +60,12 @@
       <f7-view id="main-view" navbar-through :dynamic-navbar="true" main>
         <!-- iOS Theme Navbar -->
         <f7-navbar v-if="$theme.ios">
-          <f7-nav-left>
+          <!-- <f7-nav-left>
             <f7-link icon="icon-bars" open-panel="left"></f7-link>
-          </f7-nav-left>
-          <f7-nav-center sliding>Framework7</f7-nav-center>
+          </f7-nav-left> -->
+          <f7-nav-left sliding>&nbsp;&nbsp; Admin yim</f7-nav-left>
           <f7-nav-right>
-            <f7-link icon="icon-bars" open-panel="right"></f7-link>
+            <f7-link href="/about/"><f7-icon f7="info"></f7-icon></f7-link>
           </f7-nav-right>
         </f7-navbar>
         <!-- Pages -->
@@ -73,12 +73,12 @@
           <f7-page>
             <!-- Material Theme Navbar -->
             <f7-navbar v-if="$theme.material">
-              <f7-nav-left>
+              <!-- <f7-nav-left>
                 <f7-link icon="icon-bars" open-panel="left"></f7-link>
-              </f7-nav-left>
-              <f7-nav-center sliding>Framework7</f7-nav-center>
+              </f7-nav-left> -->
+              <f7-nav-left sliding>&nbsp;&nbsp; Admin yim</f7-nav-left>
               <f7-nav-right>
-                <f7-link icon="icon-bars" open-panel="right"></f7-link>
+                <f7-link href="/about/"><f7-icon material="info"></f7-icon></f7-link>
               </f7-nav-right>
             </f7-navbar>
             <!-- Page Content -->
